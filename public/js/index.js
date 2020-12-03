5e24b7d91895897250e12707ab1d122f4f927e49
arjungarg07 <arjun.garg.cse01@gmail.com>
2020-12-04 05:19:17 +0530

    frontend fixes

--- public/js/index.js
+++ public/js/index.js
@@ -1,69 +1,70 @@
-document.getElementById("url-form").addEventListener("submit", async (e) => {
+document.getElementById('url-form').addEventListener('submit', async (e) => {
 	// let creationDate = new Date().toLocaleDateString();
 	e.preventDefault();
 	// console.log(e.target.elements[0].value);
 	const {
-	  target: {
-		elements: [{ value: originalUrl }],
-	  },
-	} = e;
-  
-	if (!validURL(originalUrl)) {
-	  console.log("Invalid Url");
-	  onInvalid();
-	} else {
-	//   console.log(elements[0].value);
-	console.log(originalUrl);
-	  let response = await fetch("/shorten", {
-		method: "POST",
-		headers: {
-		  "Content-Type": "application/json",
+		target: {
+			elements: [{ value: originalUrl }],
 		},
-		body: JSON.stringify({
-		  originalUrl,
-		}),
-	  })
-		.then((res) => res.json())
-		.catch((error) => console.log(error));
-	  if(response.success){
-		  console.log('Response:',response);
-		  document.getElementById("shorten_url").innerText = response.data;
-		  document.getElementById("shorten_url").href = response.data;
+	} = e;
+
+	if (!validURL(originalUrl)) {
+		console.log('Invalid Url');
+		onInvalid();
+	} else {
+		//   console.log(elements[0].value);
+		console.log(originalUrl);
+		let response = await fetch('/shorten', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				originalUrl,
+			}),
+		})
+			.then((res) => res.json())
+			.catch((error) => console.log(error));
+		if (response.success) {
+			console.log('Response:', response);
+			document.getElementById('shorten_url').innerText = response.data;
+			document.getElementById('shorten_url').href = response.data;
 		}
-	  document.querySelector("#copy").addEventListener("click", (e) => {
-		ClipBoard(response);
-	  });
+		document.querySelector('#copy').style.display = 'block';
+		document.querySelector('#copy').addEventListener('click', (e) => {
+			ClipBoard(response);
+		});
 	}
-  });
-  
-  function validURL(url) {
+});
+
+function validURL(url) {
 	if (
-	  url.substring(0, 4) != "http" &&
-	  url.substring(0, 4) != "HTTP" &&
-	  url.substring(0, 4) != "Http"
+		url.substring(0, 4) != 'http' &&
+		url.substring(0, 4) != 'HTTP' &&
+		url.substring(0, 4) != 'Http'
 	)
-	  return false;
+		return false;
 	else {
-	  return true;
+		return true;
 	}
-  }
-  
-  function onInvalid() {
-	document.getElementById("invalid-url").style.display = "block";
-	document.getElementById("invalid-url").innerText = "Please enter Valid Url";
+}
+
+function onInvalid() {
+	document.getElementById('invalid-url').style.display = 'block';
+	document.getElementById('invalid-url').innerText = 'Please enter Valid Url';
 	setTimeout(function () {
-	  document.getElementById("invalid-url").style.display = "none";
+		document.getElementById('invalid-url').style.display = 'none';
 	}, 2000);
-  }
-  
-  function submitForm(e) {
+}
+
+function submitForm(e) {
 	console.log(e.target);
-  }
-  
-  function ClipBoard(result) {
+}
+
+function ClipBoard(result) {
 	navigator.clipboard.writeText(result);
-	$("#copied").slideToggle(200);
+	$('#copied').slideToggle(200);
 	setTimeout(function () {
-	  $("#copied").slideToggle(200);
+		$('#copied').slideToggle(200);
 	}, 1000);
-  }
+}

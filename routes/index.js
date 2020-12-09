a241ea2e39b34b1dfb2205350dc1fd206a4b92b6
majjikishore007 <majjikishore031@gmail.com>
2020-12-09 18:19:02 +0530

    index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 const cache = require('../middlewares/cachePolicy');
 const { URLshorten ,URLredirect}= require('../controllers/url');
 
-<<<<<<< HEAD
 const {}=require("../controllers/url");
 const { signup, signin, signout } = require('../controllers/auth');
 
@@ -19,9 +18,5 @@ router.get("/signout",signout);
 
 router.post('/shorten',url.shorten);
 router.get('/:code',cache,url.redirect);
-=======
-router.post('/shorten',URLshorten);
-router.get('/:code',cache,URLredirect);
->>>>>>> 4d5353899dd212a249c2e18237c871ca02406214
 
 module.exports = router;

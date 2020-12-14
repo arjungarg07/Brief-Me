ceeb7928ca4a9f153cb8377cd4aee1fcd716e85e
majjikishore007 <majjikishore031@gmail.com>
2020-12-14 23:36:12 +0530

    resolved errors

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,38 @@ const router = require("express").Router();
 const passport = require("passport");
 const cache = require("../middlewares/cachePolicy");
 const url = require("../controllers/url");
-const googleAuth = require("../controllers/googleAuth");
+//const googleAuth = require("../controllers/googleAuth");
+const auth = require('../controllers/auth');
+
+
+require("../config/passport-setup");
+
+
+//authentation 
+router.post("/signup",auth.signup);
+router.post("/signin",auth.signin);
+router.get("/signout",auth.signout);
+
 
 router.post("/shorten", url.shorten);
 router.get("/:code", cache, url.redirect);
 
 // auth logout
-router.get("/auth/logout", googleAuth.logout);
+//router.get("/auth/logout", googleAuth.logout);
 
 // auth with google+
-router.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
+// router.get(
+//   "/auth/google",
+//   passport.authenticate("google", {
+//     scope: ["profile", "email"],
+//   })
+// );
 
 // callback route for google to redirect to
-router.get(
-  "/auth/google/redirect",
-  passport.authenticate("google"),
-  googleAuth.redirect
-);
+// router.get(
+//   "/auth/google/redirect",
+//   passport.authenticate("google"),
+//   googleAuth.redirect
+// );
 
 module.exports = router;
